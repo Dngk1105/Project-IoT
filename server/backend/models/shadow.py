b99593c99861faed1a0af311769039bdbd0edf20
Dung Ha Tuan <hatuandung2408@example.com>
2026-06-09 14:10:11 +0700

    Trả lại tên

--- server/backend/models/shadow.py
+++ server/backend/models/shadow.py
@@ -6,7 +6,9 @@ import json
 from .base import Base
 
 class DeviceEventShadow(Base):
-    """Luu trang thai cac su kien cua esp"""    
+    """Luu trang thai cac su kien cua esp"""
+    __tablename__ = "device_event_shadow"
+
     #Neu Device bi xoa trong bang devices
     #Record co devices.id se deu bi xoa
     device_id: Mapped[str] = mapped_column(String(50), ForeignKey("devices.id", ondelete="CASCADE"), primary_key=True) 
